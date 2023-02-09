29c99a292956b3a66c5c3a5d5dd9328264de592f
jeffpatriota <jefferson.patriota@outlook.com>
2023-02-09 12:24:38 -0300

    updating post

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { db } from './firebaseconnection';
-import {doc, setDoc, collection, addDoc, getDoc, getDocs} from 'firebase/firestore';
+import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc } from 'firebase/firestore';
 
 import './app.css';
 import { async } from '@firebase/util';
@@ -8,36 +8,37 @@ import { async } from '@firebase/util';
 function App() {
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
+  const [idPost, setIdPost] = useState('');
 
   const [posts, setPosts] = useState([]);
 
- async function handleAdd (){
-//   await setDoc(doc(db, "posts", "12345"), {      //o setDoc tem que especificar o documento no caso o id 12345
-//     titulo: titulo,
-//     autor: autor,
-//   })                                      //Código para gerar um id de forma manual, no caso eu coloquei o id 12345 
-//   .then(()=> {
-//     console.log("DADOS REGISTRADO NO BANCO!")
-//   })
-//   .catch((error)=>{
-//     console.log("GEROU ERRO" + error)
-//   })
+  async function handleAdd() {
+    //   await setDoc(doc(db, "posts", "12345"), {      //o setDoc tem que especificar o documento no caso o id 12345
+    //     titulo: titulo,
+    //     autor: autor,
+    //   })                                      //Código para gerar um id de forma manual, no caso eu coloquei o id 12345 
+    //   .then(()=> {
+    //     console.log("DADOS REGISTRADO NO BANCO!")
+    //   })
+    //   .catch((error)=>{
+    //     console.log("GEROU ERRO" + error)
+    //   })
     await addDoc(collection(db, "posts"), {         //o addDoc diferente do setDoc ele gera um id unico pelo proprio firebase
       titulo: titulo,
       autor: autor,
     })
-    .then(()=>{
-      console.log("DADOS REGISTRADO")
-      setAutor("");
-      setTitulo("");
-    })
-    .catch((error)=>{
-      console.log("ERRO" + error)
-    })
+      .then(() => {
+        console.log("DADOS REGISTRADO")
+        setAutor("");
+        setTitulo("");
+      })
+      .catch((error) => {
+        console.log("ERRO" + error)
+      })
   }
 
-  async function buscarPost(){
-    
+  async function buscarPost() {
+
     // const postRef = doc(db, "posts", "PbIcNz5rbF6VDdRcp88S")
 
     // await getDoc(postRef)                    //o getDoc busca o post especifico nesse caso em busquei a id PbIcNz5rbF6VDdRcp88S
@@ -51,43 +52,66 @@ function App() {
 
     const postRef = collection(db, "posts")
     await getDocs(postRef)
-    .then((snapshot)=>{
-      let lista = [];
+      .then((snapshot) => {
+        let lista = [];
 
-      snapshot.forEach((doc)=>{
-        lista.push({
-          id: doc.id,
-          titulo: doc.data().titulo,
-          autor: doc.data().autor,
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor,
+          })
         })
-      })
 
-      setPosts(lista);
-    })
-    .catch(()=>{
-      console.log("DEU ALGUM ERRO AO BUSCAR")
-    })
+        setPosts(lista);
+      })
+      .catch(() => {
+        console.log("DEU ALGUM ERRO AO BUSCAR")
+      })
   }
-  
+
+async function editarPost (){
+  const docRef = doc(db, "posts", idPost)
+  await updateDoc(docRef, {
+    titulo: titulo,
+    autor:autor
+  })
+  .then(()=>{
+    console.log("POST ATUALIZADO!")
+    setIdPost('')
+    setTitulo('')
+    setAutor('')
+  })
+  .catch(()=>{
+    console.log("ERRO AO ATUALIZAR!")
+  })
+}
 
   return (
     <div>
       <h1>ReactJS + Firebase :) </h1>
 
       <div className='container'>
+
+        <label>ID do Post</label>
+        <input
+          placeholder='Digite o ID do post que deseja atualizar' value={idPost} onChange={(e) => setIdPost(e.target.value)} /><br/>
+
         <label>Titulo:</label>
-        <textarea type="text" placeholder='Digite o titulo' value={titulo}  onChange={(e)=>setTitulo (e.target.value) }/>
+        <textarea type="text" placeholder='Digite o titulo' value={titulo} onChange={(e) => setTitulo(e.target.value)} /><br/>
         <label>Autor:</label>
-        <input type="text" placeholder='Autor do post' value={autor}  onChange={(e)=> setAutor (e.target.value)}/>
-        <button onClick={handleAdd}>Cadastrar</button>
-        <button onClick={buscarPost}>Buscar Post</button>
+        <input type="text" placeholder='Autor do post' value={autor} onChange={(e) => setAutor(e.target.value)} /><br/>
+        <button onClick={handleAdd}>Cadastrar</button><br/>
+        <button onClick={buscarPost}>Buscar Post</button> <br/>
+        <button onClick={editarPost}>Atualizar post</button>
 
         <ul>
-          {posts.map((post)=>{
-            return(
-              <li key={post.i}>
-                <span> Titulo: {post.titulo}</span><br/>
-                <span> Autor: {post.autor}</span><br/><br/>
+          {posts.map((post) => {
+            return (
+              <li key={post.id}>
+                <strong>ID: {post.id}</strong><br/>
+                <span> Titulo: {post.titulo}</span><br />
+                <span> Autor: {post.autor}</span><br /><br />
               </li>
             )
           })}
